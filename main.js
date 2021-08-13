5e74bb67e84add2d5cd0ce53eb0ccf77cf78a3e8
Ynardy <ya.lutkova@gmail.com>
2021-08-13 21:55:03 +0600

    first upload

---
+++ main.js
@@ -0,0 +1,12 @@
+$('.offers__carousel').slick({
+    infinite: true,
+    dots: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    center:true,
+
+
+});
+
+document.querySelector('.slick-prev').textContent = '';
+document.querySelector('.slick-next').textContent = '';
---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+$('.reviews-carousel').slick({
+
+});
+
+document.querySelector('.slick-prev').textContent = '';
+document.querySelector('.slick-next').textContent = '';
+
+AOS.init();
